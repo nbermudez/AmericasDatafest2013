b1adc9881c7d994ff30769624562d1c07ccdfa8f
Néstor A. Bermúdez S <nestor.bermudez@unitec.edu>
2013-11-02 11:22:29 -0600

    New Project

--- Potato/Potato/Global.asax.cs
+++ Potato/Potato/Global.asax.cs
@@ -22,6 +22,7 @@ namespace Potato
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
         }
     }
 }
--- Potato/Potato/Properties/AssemblyInfo.cs
+++ Potato/Potato/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
-[assembly: Guid("1c5cdb7b-8cc4-4ea2-9858-1449344c9da4")]
+[assembly: Guid("eb1b76f1-46ed-46c9-9382-a0d131efaec1")]
 
 // La información de versión de un ensamblado consta de los siguientes cuatro valores:
 //
